b780b61958438790213c0fd35aff643bcc724543
Bernhard Stöffler <bernie@bernie-tablet>
2021-11-15 15:50:28 +0100

    +wificredential.h example

---
+++ include/wificredentials.h
@@ -0,0 +1,18 @@
+/**
+ * @file wificredentials.h
+ * @author Bernhard Stöffler
+ * @brief 
+ * @version 0.1
+ * @date 2021-11-15
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#ifndef __WIFICREDENTIALS_H_
+#define __WIFICREDENTIALS_H_
+
+#define STASSID "Name"
+#define STAPSWD "Password"
+
+#endif
