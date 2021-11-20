984184b6675ce3e0e13d9ce89020ca6bf2029fd6
Bernhard Stöffler <bernie@bernie-debian>
2021-11-20 23:39:21 +0100

    sampleProgram: adapted to new framework

--- src/programs/sampleProgram.cpp
+++ src/programs/sampleProgram.cpp
@@ -9,21 +9,18 @@
  * 
  */
 
-#include "programs/sampleProgram.h"
+#include "zylProg.h"
+#pragma message: Obnoxious
 
-int sampleProgram::init(){
-	m_Color = CRGB::Red;
-	return 0;
-}
-
-void sampleProgram::render(){
-	for(int x=0; x<X_RES; x++)
-		for(int y=0; y<Y_RES; y++)
-			m_FB = m_Color;
-}
-
-void sampleProgram::input(uint8_t x, uint8_t y, uint8_t z){
-	m_Color.setRGB(x, y, z);
-}
-
-static sampleProgram(g_Zpm); //add itself to global program manager
+class : zylProg{
+public:
+	int init(){
+		m_FB = zylProgManager::getColor(0);
+		return 0;
+	}
+	void render(){
+		for(int x=0; x<X_RES; x++)
+			for(int y=2; y<Y_RES-3; y++)
+				m_FB(x,y).setHue((millis()/100)%255);
+	}
+} sampleProgram;
